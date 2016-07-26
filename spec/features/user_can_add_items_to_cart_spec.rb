902013552160df8b3ed0432793adf8c7afe9367d
Tommasina <tommasina1@gmail.com>
2016-07-26 11:48:26 -0600

    resolve trailing end spaces

--- spec/features/user_can_add_items_to_cart_spec.rb
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 RSpec.feature "Visitor can add items to cart" do
   scenario "they click to add an item to their cart" do 
-  Creature.create(
-    name: "Hippogriff",
-    price: "500.00"
-    )
+    Creature.create(
+      name: "Hippogriff",
+      price: "500.00"
+      )
 
-    visit root_path
-    expect(page).to have_content("Cart: 0")
+      visit root_path
+      expect(page).to have_content("Cart: 0")
 
-    click_button "Sponsor me"
+      click_button "Sponsor me"
 
-    expect(page).to have_content("You now have 1 sponsored Hippogriff")
-    expect(page).to have_content("Cart: 1")
+      expect(page).to have_content("You now have 1 sponsored Hippogriff")
+      expect(page).to have_content("Cart: 1")
 
-    click_button "Sponsor me"
+      click_button "Sponsor me"
 
-    expect(page).to have_content("You now have 2 sponsored Hippogriffs")
-    expect(page).to have_content("Cart: 2")
+      expect(page).to have_content("You now have 2 sponsored Hippogriffs")
+      expect(page).to have_content("Cart: 2")
   end
 end
