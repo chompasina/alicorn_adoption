4fb9003c6222564fbeda59d202d589285c5b2a49
Chris Concannon <concach00@gmail.com>
2016-07-26 15:11:32 -0600

    Format Images as PNG

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -2,5 +2,6 @@ class Creature < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
   
-  belongs_to :type
+  belongs_to :type  
+  # validates :type, presence: true
 end
