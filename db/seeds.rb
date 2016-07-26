64eb00f8dd4a86c5e3572702ff70594c99cb4204
Chris Concannon <concach00@gmail.com>
2016-07-26 11:57:56 -0600

    hound styling fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,27 +28,28 @@ class Seed
   end
   
   def creatures
-    { "Terrestrial" => [
-        ["Basilisk", 350.00],
-        ["Centaur", 200.00],
-        ["Dwarf", 150.00],
-        ["Elf", 175.00],
-        ["Gnome", 175.00],
-        ["Leprechaun", 125.00],
-        ["Minotaur", 300.00],
-        ["Unicorn", 800.00]
+    { "Terrestrial" =>
+      [ [ "Basilisk", 350.00 ],
+        [ "Centaur", 200.00 ],
+        [ "Dwarf", 150.00 ],
+        [ "Elf", 175.00 ],
+        [ "Gnome", 175.00 ],
+        [ "Leprechaun", 125.00 ],
+        [ "Minotaur", 300.00 ],
+        [ "Unicorn", 800.00]
       ],
       "Aquatic" => [
         ["Mermaid", 400.00]
       ],
-      "Aerial" => [
-        ["Alicorn", 650.00],
+      "Aerial" =>
+      [ ["Alicorn", 650.00],
         ["Dragon", 1050.00],
         ["Fairy", 75.00],
         ["Pegasus", 600.00],
         ["Phoenix", 400.00],
         ["Pixie", 50.00]
-    ]}
+      ]
+    }
   end
   
 end
