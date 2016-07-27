8627e6d1975df75c789cb41ae8622d7d0dd5686c
Tommasina <chompasina@users.noreply.github.com>
2016-07-26 19:33:49 -0600

    Update user_can_add_items_to_cart_spec.rb

--- spec/features/user_can_add_items_to_cart_spec.rb
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper'
-#
-# As a visitor
-# When I visit any page with an item on it
-# I should see a link or button for "Add to Cart"
-# When I click "Add to cart" for that item
-# And I click a link or button to view cart
-# And my current path should be "/cart"
-# And I should see a small image, title, description and price for the item I just added
-# And there should be a "total" price for the cart that should be the sum of all items in the cart
 
 RSpec.feature "Visitor can add an item to cart" do
    scenario "when on the homepage, visitor clicks to add an item to cart" do 
