8a72cceb7cda7506c468779a2d2ed51325177da3
Tommasina <chompasina@users.noreply.github.com>
2016-07-26 19:34:38 -0600

    Update user_can_add_items_to_cart_spec.rb

--- spec/features/user_can_add_items_to_cart_spec.rb
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Visitor can add an item to cart" do
    scenario "when on the homepage, visitor clicks to add an item to cart" do 
-    creature = FactoryGirl.create(:creature)
+      creature = FactoryGirl.create(:creature)
 
       visit root_path
       expect(page).to have_link("Cart: 0")
@@ -20,7 +20,7 @@ RSpec.feature "Visitor can add an item to cart" do
       # expect(page).to have_content(creature.description)
       expect(page).to have_css(:img)
       # expect(page).to have_content(order.total)
- end
+   end
 end
       
   # scenario "when they are on the homepage, they can click to add an item to their cart" do 
