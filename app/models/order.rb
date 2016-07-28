7d34037ed7712537cfcdb504a548638c12ad96fa
Tommasina <chompasina@users.noreply.github.com>
2016-07-28 14:08:21 -0600

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   def self.total_price(cart)
     total_price = 0
-    if !cart.nil?
+    unless cart.nil?
       cart.each do |id, qty| 
         total_price += qty * Creature.find(id).price
       end 
