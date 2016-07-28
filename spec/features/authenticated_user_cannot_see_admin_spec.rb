4495f27ed35e59daebd8248d28b38b396f3781af
Tommasina <tommasina1@gmail.com>
2016-07-28 15:45:14 -0600

    add three tests: 1) admin can login 2) authenticated user cannot see admin privileges 3) user can place order and changed name of user can checkout to user can view cart.

---
+++ spec/features/authenticated_user_cannot_see_admin_spec.rb
@@ -0,0 +1,41 @@
+# An authenticated user and the ability to add an admin user
+# As an Authenticated User
+# I cannot view another user's' private data (current or past orders, etc)
+# I cannot view the administrator screens or use admin functionality
+# I cannot make myself an admin
+
+require 'rails_helper'
+
+RSpec.feature "Authenticated user cannot see admin privileges" do
+  context "user is authenticated" do
+    scenario "user cannot see other user or admin pages" do 
+      creature = FactoryGirl.create(:creature)
+      authenticated_user = User.create!(username: "Casey", password: "password", email: "casey@gmail.com", admin: false)
+      other_user = User.create!(username: "Andrew", password: "password", email: "andrew@gmail.com", admin: false)
+      admin = User.create!(username: "admin", password: "admin", email: "admin@gmail.com", admin: true)
+      
+      visit root_path
+      click_on "Login"
+      fill_in "session_username", with: "Casey"
+      fill_in "session_password", with: "password"
+      click_button "Login"
+      
+      expect(authenticated_user.admin).to eq(false)
+      expect(page).to_not have_content("Logged in as admin")
+      expect(page).to_not have_content("Logged in as Andrew")
+      expect(page).to have_content("Logged in as Casey")
+      expect(current_path).to_not eq('/admin/dashboard')
+      
+      visit root_path
+      expect(page).to_not have_content("Add Creatures")
+      expect(page).to_not have_content("Add Admin")
+      
+      click_button "Sponsor me"
+      visit '/cart'
+      click_button "Checkout"
+      
+      expect(page).to have_content("Casey's Past Orders")
+      expect(page).to_not have_content("Andrew's Past Orders")
+    end
+  end
+end
---
+++ spec/features/user_can_place_order_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+# Background: An existing user and a cart with items
+# As a visitor
+# When I add items to my cart
+# And I visit "/cart"
+# And I click "Login or Register to Checkout"
+# Then I should be required to login
+# When I am logged in I should be taken back to my cart
+# And when I click "Checkout"
+# Then the order should be placed
+# And my current page should be "/orders"
+# And I should see a message "Order was successfully placed"
+# And I should see the order I just placed in a table
+
+RSpec.feature "Existing user has items in cart" do
+  scenario "they can place an order" do
+    creature = FactoryGirl.create(:creature)
+    user = User.create!(username: "Casey", password: "password", email: "casey@gmail.com")
+    
+    visit root_path
+    click_button "Sponsor me"
+    visit '/cart'
+    click_on "Login or Register to Checkout"
+    
+    expect(current_path).to eq(login_path)
+    
+    fill_in "session_username", with: "Casey"
+    fill_in "session_password", with: "password"
+    click_button "Login"
+    
+    expect(current_path).to eq('/cart')
+    
+    click_button "Checkout"
+    
+    expect(current_path).to eq(order_path)
+    expect(page).to have_content("Order was successfully placed")
+    expect(page).to have_content("Your Order:")
+    expect(page).to have_content("Sponsorships: #{creature.name}")
+    expect(page).to have_content("Quantity: 1"
+    expect(page).to have_content("Total Price:")
+  end
+end
