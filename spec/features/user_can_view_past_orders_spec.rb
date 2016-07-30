e69e8a253ce33f833d23e40c7afc5c3e5a1bc4a5
Chris Concannon <concach00@gmail.com>
2016-07-30 12:20:26 -0600

    test writing begins

--- spec/features/user_can_view_past_orders_spec.rb
+++ spec/features/user_can_view_past_orders_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature "Authenticated user can view past orders" do
+  land_type = Type.create!(name: "terrestrial")
+  creature_1 = land_type.creatures.create!(name: "Unicorn", price: 200.00)
+  creature_2 = land_type.creatures.create!(name: "Centaur", price: 150.00)
+  
+  user1 = User.create!(username: "Casey", password: "password", email: "casey@gmail.com")
+  
+  user1_order1 = user1.orders.create
+  order1_items = user1_order1.creatures_orders.create!(
+    creature_id: creature_1.id, 
+    quantity: 2
+  )
+  
   context "User has multiple previous orders" do
-    scenario "they see their page with their order history" do
-      land_type = Type.create!(name: "terrestrial")
-      creature_1 = land_type.creatures.create!(name: "Unicorn", price: 200.00)
-      creature_2 = land_type.creatures.create!(name: "Centaur", price: 150.00)
-      
-      user1 = User.create!(username: "Casey", password: "password", email: "casey@gmail.com")
-      
-      user1_order1 = user1.orders.create
-      order1_items = user1_order1.creatures_orders.create!(
-        creature_id: creature_1.id, 
-        quantity: 2
-      )
+    scenario "they see their page with their order history" do 
     
       user1_order2 = user1.orders.create
       order1_items = user1_order2.creatures_orders.create!(
@@ -45,5 +46,8 @@ RSpec.feature "Authenticated user can view past orders" do
       expect(page).to have_content(user1_order2.id)
       expect(page).to_not have_content(user2_order1.id)
     end
+  end
+    
+  scenario "they click on a link to view their order" do
   end
 end
