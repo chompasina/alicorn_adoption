1ccb43db9c272e034bd4625d89d3d6ba49a828fc
Tommasina <tommasina1@gmail.com>
2016-07-30 19:00:16 -0600

    user model test complete and passing

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:order).should be_valid
+  end
+
+  it "has order details" do 
+    Order.new()
+    
+    <% order.order_details.each do |key, value| %>
+      <tr>
+        <th><%= link_to key, creature_path(find_creature(key)) %></th>
+        <th><%= value.last %></th>
+        <th><%= number_to_currency(value.first) %></th>
+      </tr>
+  
 end
