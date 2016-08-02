7a9958de7bfabe938d108d4546e7a4f88a65f27c
Tommasina <tommasina1@gmail.com>
2016-08-01 18:17:38 -0600

    Revert "Merge branch 'user_default_not_admin_#53'"
    This reverts commit 430bdab797437a6a1b9473b6e851e695fbd67b91, reversing
    changes made to 6b5fb6f24eb53d9e504a30c57ea2ca40c5bd8018.
    
    Reversing previous commit to pull master into local first.

--- db/migrate/20160801223536_change_user_admin_default.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserAdminDefault < ActiveRecord::Migration
-  def change
-    change_column_default :users, :admin, false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801223536) do
+ActiveRecord::Schema.define(version: 20160730183816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(version: 20160801223536) do
     t.string   "username"
     t.string   "password_digest"
     t.string   "email"
-    t.boolean  "admin",           default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.boolean  "admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "creatures", "types"
