c23ef8b27e8878573aa2dfce747613e4106c3347
Chris Concannon <concach00@gmail.com>
2016-08-02 13:38:35 -0600

    migration to assign admin value to false upon user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  after_initialize :default_values 
 
   validates :email, presence: true
   validates :password, presence: true
@@ -8,11 +7,5 @@ class User < ActiveRecord::Base
   has_many :orders
   
   def to_param
-  end
-    
-  private
-
-  def default_values
-    self.admin ||= false
   end
 end
